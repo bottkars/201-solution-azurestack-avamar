317bcb7f6565474e09d67be7b23788d2496a8c47
Bott <Karsten_Bott@Dell.com>
2020-05-23 19:19:59 +0200

    latest

--- scripts/configure-ave-azs.sh
+++ scripts/configure-ave-azs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-exec &> >(tee -a "install.log")
+exec &> >(tee -a ./install.log)
+exec 2>&1
 set -ex
 function retryop()
 {
--- scripts/deploy_base.sh
+++ scripts/deploy_base.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp *.sh /root
+echo "forking to configure-ave-azs.sh, monitor install.log for root or AVIGUI"
 su root -c "nohup /root/configure-ave-azs.sh >/dev/null 2>&1 &"
